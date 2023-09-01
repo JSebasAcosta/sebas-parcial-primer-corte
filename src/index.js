d8be69b6f0e006c80299b131c353ff792d32709f
JSebasAcosta <js09acosta@gmail.com>
2023-08-31 21:28:11 -0500

    Parcial 1 Juan Acosta

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import RickAndMortyService from './service';
 
 
 // acá deberás crear una instancia del servicio RickAndMortyService
-// const service = new RickAndMortyService();
+const service = new RickAndMortyService();
 
 // esta función debe encargarse de obtener el elemento contenedor
 // y agregar los personajes obtenidos por el API, deberás llamar tu función getAllCharacters
@@ -14,6 +14,16 @@ import RickAndMortyService from './service';
 function createCharacterList() {
     // llamar primero createCharacterCard(character);
     // llamar segundo addCharacterListeners(character);
+
+    const columns = document.querySelector('.character-list');
+    service.getAllCharacters()
+        .then(characters=>{
+        characters.forEach(character => {
+            const characterCard = createCharacterCard(character);
+            columns.innerHTML += characterCard;
+            addCharacterListeners(character, columns);
+        });
+    })
 }
 
 // esta función debe devolver la estructura html en string de tu personaje ejemplo
@@ -27,13 +37,34 @@ function createCharacterList() {
 
 // valor (1 punto) HTML
 
-function createCharacterCard(character) {}
+function createCharacterCard(character) {
 
 // esta función deberá obtener todos los personajes y deberá agregarles un evento de click
 // cuando se seleccione un personaje debe decir hola soy 'nombre personaje', recuerda que puedes obtener
 // el elemento target de un evento y así obtener sus propiedades
+return `
+        <div class="character">
+            <div class="personaje">
+                <img src="${character.image}" alt="${character.name}">
+            </div>
+            <div class="descripcion">
+                <h1>${character.name}</h1>
+                <h2>${character.status} - ${character.species}</h2>
+                <p>Last known location:</p>
+                <h2>${character.location}</p>
+                <p>First seen in:</p>
+                <h2>${character.firstSeen}</p>
+            </div>
+        </div>
+      `;
+}
 
-function addCharacterListeners(character) {}
+function addCharacterListeners(character, columns) {
+
+    columns.addEventListener('click', () => {
+        alert(`Hola mi nombre es ${character.name}`);
+    });
+}
 
 
 // por último se llama la función y se renderiza la vista
